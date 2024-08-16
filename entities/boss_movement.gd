846860da7954f37462961e2a272350bc93e6491a
SecretJuice <connor.m.robb33@gmail.com>
2024-08-16 17:41:05 -0600

    feat: spider boss art and animation

---
+++ entities/boss_movement.gd
@@ -0,0 +1,49 @@
+extends AnimatableBody2D
+
+var dir = Vector2(0,0)
+
+var t = PI / 2
+
+@export var movementSpeed = 300 
+
+var legHips: Array[Node2D] = []
+var legOrginalRotations: Array[float] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	legHips.push_back($HitboxComponent/LegHip)
+	legHips.push_back($HitboxComponent/LegHip2)
+	legHips.push_back($HitboxComponent/LegHip3)
+	legHips.push_back($HitboxComponent/LegHip4)
+	legHips.push_back($HitboxComponent/LegHip5)
+	legHips.push_back($HitboxComponent/LegHip6)
+	
+	legOrginalRotations.push_back(legHips[0].rotation)
+	legOrginalRotations.push_back(legHips[1].rotation)
+	legOrginalRotations.push_back(legHips[2].rotation)
+	legOrginalRotations.push_back(legHips[3].rotation)
+	legOrginalRotations.push_back(legHips[4].rotation)
+	legOrginalRotations.push_back(legHips[5].rotation)
+
+func _animateLegs(dir: Vector2):
+	
+	for i in 6:
+		var offset = dir.x * .4
+		legHips[i].rotation = legOrginalRotations[i] + offset
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	
+	t += delta
+	
+	
+	dir = Vector2(sin(t), 0)
+	
+	position += dir * movementSpeed
+	
+	_animateLegs(dir)
