621aaca43c513de71706670c244e47cd5298b225
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-16 12:56:29 -0600

    renamed to snake case

---
+++ components/health_component.gd
@@ -0,0 +1,32 @@
+extends Node2D
+class_name HealthComponent
+
+signal died
+signal changed(old_amount: float, new_amount: float)
+
+@export var MAX_HEALTH: float = 10.0
+@export var remove_on_died: bool = false
+@export var invincible: bool = false
+
+var health: float
+var alive: bool
+
+func _ready():
+	alive = true
+	health = MAX_HEALTH
+
+func damage(amount: float):
+	if not alive:
+		health = 0
+		return
+	if invincible:
+		return
+	var old_amount = health
+	health -= amount
+	changed.emit(old_amount, health)
+	if health <= 0:
+		health = 0
+		alive = false
+		died.emit()
+		if remove_on_died:
+			get_parent().queue_free()
--- components/hitbox_component.gd
+++ components/hitbox_component.gd
@@ -6,6 +6,6 @@ signal hit
 @export var health_component: HealthComponent
 
 func damage(amount: float):
-    if health_component:
-        health_component.damage(amount)
-    hit.emit()
+	if health_component:
+		health_component.damage(amount)
+	hit.emit()
