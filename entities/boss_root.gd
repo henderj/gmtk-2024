235864aaf85a046bb44160298cd60d6abeea1c42
SecretJuice <connor.m.robb33@gmail.com>
2024-08-17 10:27:02 -0600

    feat: boss telegraph expressions

--- entities/boss_root.gd
+++ entities/boss_root.gd
@@ -5,26 +5,48 @@ signal die
 signal initialize(max: float, val: float, color: Color)
 
 @onready var body = $AnimatableBody2D
+@onready var timer = $Timer
+@onready var sprite = $AnimatableBody2D/Sprite2D
+
+@export var attackPatterns: Array[PackedScene] = []
+@export var facialExpressions: Array[Texture2D] = []
+@export var neutralExpression: Texture2D
+
+var currentMoveIndex = 0
+
+func telegraph():
+	currentMoveIndex = randi_range(0, attackPatterns.size() - 1)
+	sprite.texture = facialExpressions[currentMoveIndex]
+	
+	await get_tree().create_timer(1.0).timeout
+	
+	attack()
+
+func attack():
+	var pattern = attackPatterns[currentMoveIndex].instantiate()
+	
+	if not pattern == null:
+		get_tree().current_scene.add_child(pattern)
+		pattern.position = position
+	
+	sprite.texture = neutralExpression
+	
+	timer.start(randf_range(3.0, 6.0))
+	
+func _ready():
+	timer.start(randf_range(3.0, 6.0))
+	neutralExpression = sprite.texture
 
 func TakeDamage(old: float, new: float):
 	emit_signal("take_damgage", old, new)
 
 func Die():
 	emit_signal("die")
+	queue_free()
 	
 func _initializeHealthBar(max: float, val: float, color: Color):
 	initialize.emit(max, val, color)
 	print("BOSS INITIALIZED")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = body.global_position
-
-
-func _on_health_component_initialize(max, val, color):
-	pass # Replace with function body.
