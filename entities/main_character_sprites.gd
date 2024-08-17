6439dc6873173ab6895acaa448900b2b744feef3
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-17 10:28:10 -0600

    added bullet and entity layers

--- entities/main_character_sprites.gd
+++ entities/main_character_sprites.gd
@@ -4,22 +4,25 @@ extends Node2D
 @export var mid_head_tilt: float = 0
 @export var min_head_tilt: float = -20
 
-var originalScale: Vector2
-
-func _ready():
-	originalScale = scale
-	
+@onready var max_head_tilt_rads = deg_to_rad(max_head_tilt)
+@onready var mid_head_tilt_rads = deg_to_rad(mid_head_tilt)
+@onready var min_head_tilt_rads = deg_to_rad(min_head_tilt)
+@onready var originalScale: Vector2 = scale
 
 func calc_head_tilt(angle: float) -> float:
-	return mid_head_tilt # until we get this working lol
+	#return mid_head_tilt # until we get this working lol
 	if angle > 0 and angle <= PI / 2: # bottom right
-		return lerpf(mid_head_tilt, min_head_tilt, angle / (PI / 2))
+		print(angle / (PI / 2))
+		return lerpf(mid_head_tilt_rads, min_head_tilt_rads, angle / (PI / 2))
 	if angle > PI / 2: # bottom left
-		return lerpf(min_head_tilt, mid_head_tilt, (angle - PI / 2 ) / (PI / 2))
+		print((angle - PI / 2 ) / (PI / 2))
+		return lerpf(min_head_tilt_rads, mid_head_tilt_rads, (angle - PI / 2 ) / (PI / 2))
 	if angle < 0 and angle >= -PI / 2: # top right:
-		return lerpf(mid_head_tilt, max_head_tilt, angle / (-PI / 2))
+		print(angle / (-PI / 2))
+		return lerpf(mid_head_tilt_rads, max_head_tilt_rads, angle / (-PI / 2))
 	else: # angle < -PI / 2: # top left
-		return lerpf(max_head_tilt, mid_head_tilt, (angle + PI / 2) / (-PI / 2))
+		print((angle + PI / 2) / (-PI / 2))
+		return lerpf(max_head_tilt_rads, mid_head_tilt_rads, (angle + PI / 2) / (-PI / 2))
 
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
