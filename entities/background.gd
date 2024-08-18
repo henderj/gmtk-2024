a24770bdf25ccc606a2ffaea688324d1f92fae81
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-17 21:37:33 -0600

    background 1

---
+++ entities/background.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var player: Node2D
+@export var damping: float = 0.1
+
+var center: Vector2
+
+func _ready():
+	center = get_viewport_rect().size / 2
+
+func _process(delta):
+	if player:
+		var player_dist = player.global_position - center
+		var view_offset = player_dist * -damping
+		global_position = view_offset
